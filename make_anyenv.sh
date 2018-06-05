26235975694a44474eb3a4bb6bbf7253cffb6a7f
yfujii01 <33822726+yfujii01@users.noreply.github.com>
2018-06-05 12:43:44 +0900

    Update make_anyenv.sh

--- make_anyenv.sh
+++ make_anyenv.sh
@@ -3,4 +3,4 @@ git clone https://github.com/riywo/anyenv ~/.anyenv
 echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(anyenv init -)"' >> ~/.bashrc
 
-source .bashrc
+source ~/.bashrc
